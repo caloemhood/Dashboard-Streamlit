572435fda285b208e3895d300dac8acf22c11072
caloemhood <161462023+caloemhood@users.noreply.github.com>
2024-03-05 20:42:00 +0700

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+merged_df = pd.read_csv('all_data.csv')
+st.set_page_config(
+    page_title="Product Analysis",
+)
+
+# Pilihan menu di sidebar
+selected = st.sidebar.radio('Select Option', ['Analisis Tren Penjualan', 'Pengaruh Rating Produk dan Penjualan'])
+# Menampilkan konten sesuai pilihan di sidebar
+if selected == 'Analisis Tren Penjualan':
+  # Analisis Tren Penjualan Waktu ke Waktu
+# Convert 'order_purchase_timestamp' column to datetime format
+merged_df['order_purchase_timestamp'] = pd.to_datetime(merged df['order_purchase_timestamp'])
+# Calculate monthly sales
+monthly_sales = merged_df.groupby(pd.to_datetime(merged_df['order_purchase_timestamp']).dt.to_period('M')).size()
+
+# Plot monthly sales using Streamlit
+st.line_chart(monthly_sales)
+
+# Add title and labels
+st.title('Tren Penjualan Keseluruhan dari Waktu ke Waktu')
+st.xlabel('Tanggal')
+st.ylabel('Total Penjualan')
